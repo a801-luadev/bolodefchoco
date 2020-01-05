ebb5971a00983de2b6602a94cec1c8b9dc03c24a
Lautenschlager-id <tai.romani@hotmail.com>
2020-01-05 13:22:55 -0300

    [build] [host] update

--- builds/05_01_20.lua
+++ builds/05_01_20.lua
@@ -10986,84 +10986,82 @@ local tribeModes = function(modeName)
 			local encode
 		
 			local escape_char_map = {
-		 [ "\\" ] = "\\\\",
-		 [ "\"" ] = "\\\"",
-		 [ "\b" ] = "\\b",
-		 [ "\f" ] = "\\f",
-		 [ "\n" ] = "\\n",
-		 [ "\r" ] = "\\r",
-		 [ "\t" ] = "\\t",
+				[ "\\" ] = "\\\\",
+				[ "\"" ] = "\\\"",
+				[ "\b" ] = "\\b",
+				[ "\f" ] = "\\f",
+				[ "\n" ] = "\\n",
+				[ "\r" ] = "\\r",
+				[ "\t" ] = "\\t",
 			}
 		
 			local escape_char_map_inv = { [ "\\/" ] = "/" }
 			for k, v in pairs(escape_char_map) do
-		 escape_char_map_inv[v] = k
+				escape_char_map_inv[v] = k
 			end
 		
 		
 			local function escape_char(c)
-		 return escape_char_map[c] or string.format("\\u%04x", c:byte())
+				return escape_char_map[c] or string.format("\\u%04x", c:byte())
 			end
 		
 		
 			local function encode_nil(val)
-		 return "null"
+				return "null"
 			end
 		
 		
 			local function encode_table(val, stack)
-		 local res = {}
-		 stack = stack or {}
+				local res = {}
+				stack = stack or {}
 		
-		 -- Circular reference?
-		 if stack[val] then error("circular reference") end
+				-- Circular reference?
+				if stack[val] then error("circular reference") end
 		
-		 stack[val] = true
+				stack[val] = true
 		
-		 if val[1] ~= nil or next(val) == nil then
-				-- Treat as array -- check keys are valid and it is not sparse
-				local n = 0
-				for k in pairs(val) do
-			 if type(k) ~= "number" then
-					error("invalid table: mixed or invalid key types")
-			 end
-			 n = n + 1
+				if val[1] ~= nil or next(val) == nil then
+					-- Treat as array -- check keys are valid and it is not sparse
+					local n = 0
+					for k in pairs(val) do
+						if type(k) ~= "number" then
+							error("invalid table: mixed or invalid key types")
+						end
+						n = n + 1
+					end
+					if n ~= #val then
+						error("invalid table: sparse array")
+					end
+					-- Encode
+					for i, v in ipairs(val) do
+						table.insert(res, encode(v, stack))
+					end
+					stack[val] = nil
+					return "[" .. table.concat(res, ",") .. "]"
+				else
+					-- Treat as an object
+					for k, v in pairs(val) do
+						if type(k) ~= "string" then
+							error("invalid table: mixed or invalid key types")
+						end
+						table.insert(res, encode(k, stack) .. ":" .. encode(v, stack))
+					end
+					stack[val] = nil
+					return "{" .. table.concat(res, ",") .. "}"
 				end
-				if n ~= #val then
-			 error("invalid table: sparse array")
-				end
-				-- Encode
-				for i, v in ipairs(val) do
-			 table.insert(res, encode(v, stack))
-				end
-				stack[val] = nil
-				return "[" .. table.concat(res, ",") .. "]"
-		
-		 else
-				-- Treat as an object
-				for k, v in pairs(val) do
-			 if type(k) ~= "string" then
-					error("invalid table: mixed or invalid key types")
-			 end
-			 table.insert(res, encode(k, stack) .. ":" .. encode(v, stack))
-				end
-				stack[val] = nil
-				return "{" .. table.concat(res, ",") .. "}"
-		 end
 			end
 		
-		
 			local function encode_string(val)
-		 return '"' .. val:gsub('[%z\1-\31\\"]', escape_char) .. '"'
+				return '"' .. val:gsub('[%z\1-\31\\"]', escape_char) .. '"'
 			end
 		
 		
 			local function encode_number(val)
-		 -- Check for NaN, -inf and inf
-		 if val ~= val or val <= -math.huge or val >= math.huge then
-				error("unexpected number value '" .. tostring(val) .. "'")
-		 end
-		 return string.format("%.14g", val)
+				-- Check for NaN, -inf and inf
+				if val ~= val or val <= -math.huge or val >= math.huge then
+					error("unexpected number value '" .. tostring(val) .. "'")
+				end
+				return string.format("%.14g", val)
 			end
 		
 		
@@ -11523,7 +11521,7 @@ local tribeModes = function(modeName)
 		end
 		
 		eventTextAreaCallback = function(id, name, data)
-			system.saveFile(data, ((id == 69 and 3) or (id == 666 and 4) or (id == 6969 and 5)))
+			system.saveFile(data, id)
 			tfm.exec.chatMessage("[shades_id] Save file @" .. id)
 		end
 		
@@ -11536,7 +11534,7 @@ local tribeModes = function(modeName)
 					listenerData[#listenerData + 1] = msg
 		
 					if listener == 0 then
-						system.saveFile(table.concat(listenerData), 4)
+						--system.saveFile(table.concat(listenerData), 4)
 					end
 					return
 				end

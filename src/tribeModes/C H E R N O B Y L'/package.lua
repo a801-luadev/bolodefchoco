4ea536dcd94b1ae820e7e9a55401ca1558a59a6e
Lautenschlager-id <tai.romani@hotmail.com>
2020-01-12 18:39:56 -0300

    [build] add new tribe

---
+++ src/tribeModes/C H E R N O B Y L'/package.lua
@@ -0,0 +1,9 @@
+{
+	name = "*\3C H E R N O B Y L'",
+	authors = { "Bolodefchoco#0000" },
+	owners = { "Juuzou#8668", "Nanny#8819", "Biellnight#0415", "Otaku_foxy#0000" },
+	alias = {
+		isTribeHouse = false,
+		modeName = "perguntas"
+	}
+}

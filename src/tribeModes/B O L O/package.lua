659ea9b6fb7dc6884b406195853d79078e5059fb
Lautenschlager <tai.romani@hotmail.com>
2020-01-17 15:36:59 -0300

    add new tribe

---
+++ src/tribeModes/B O L O/package.lua
@@ -0,0 +1,10 @@
+{
+	name = "*\3B O L O",
+	authors = { "Bolodefchoco#0000" },
+	owners = { "Txnene#0000", "Lil_ever#0000", "Alessio#2918", "Bardi#9940", "Danielito#7675", "Catallena#9690" },
+	alias = {
+		isTribeHouse = false,
+    community = "es",
+		modeName = "perguntas"
+	}
+}

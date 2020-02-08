1a43621819d0ced2f1e194cbda7150f78b947d6e
Lautenschlager-id <tai.romani@hotmail.com>
2020-02-08 00:39:03 -0300

    [build] add new tribe

---
+++ src/tribeModes/The impossible is just an opinion/package.lua
@@ -0,0 +1,9 @@
+{
+	name = "*\3The impossible is just an opinion",
+	authors = { "Bolodefchoco#0000" },
+	owners = { "K_sy#0000" },
+	alias = {
+		isTribeHouse = false,
+		modeName = "perguntas"
+	}
+}

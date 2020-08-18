ed4e30127be321a114399b832a15cd973132fc8e
fireball3838 <69806814+fireball3838@users.noreply.github.com>
2020-08-19 02:31:40 +0500

    Update package.lua

--- src/tribeModes/Lost Heaven/package.lua
+++ src/tribeModes/Lost Heaven/package.lua
@@ -1,5 +1,5 @@
 {
 	name = "*\3Lost Heaven",
 	authors = { "Keras#9975" },
-	owners = { "Naomikio#0000", "Keras#9975", "Fireball#3838", "Sheogorath#8725", "Felixe96#0000","Kawaymixare#0000", "Missellik#5355", "Voimyasatani#6041", "Buterbrot#9081", "Floweystyle#8410", "Juzya#8075", "Istyb#0000" }
+	owners = { }
 }

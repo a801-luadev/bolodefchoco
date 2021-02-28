fa3ae642bfc4468b1e2e516400fccc535ccd08b5
Lautenschlager <tai.romani@hotmail.com>
2021-02-28 16:01:47 -0300

    [build] add new tribe

---
+++ src/tribeModes/evil rising/package.lua
@@ -0,0 +1,10 @@
+{
+	name = "*\3evil rising",
+	authors = { "Bolodefchoco#0015" },
+	owners = { "Max#3652", "Cierne#0000", "Khchul#6543" },
+	alias = {
+		isTribeHouse = false,
+		community = "en",
+		modeName = "perguntas"
+	}
+}
